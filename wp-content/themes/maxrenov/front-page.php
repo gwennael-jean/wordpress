40ab8e9d1b9dea661d114eb08bee74c3c66a51ff
Gwennael Jean <gwennael.jean@gmail.com>
2022-08-19 23:36:23 +0200

    feat: préparation de plusieurs template

--- wp-content/themes/maxrenov/front-page.php
+++ wp-content/themes/maxrenov/front-page.php
@@ -1,4 +1,13 @@
 <?php get_header(); ?>
+
     <?php get_template_part( 'parts/home-jumbotron' ); ?>
-    <h1>Coucou</h1>
+
+    <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+
+        <h1><?php the_title(); ?></h1>
+
+        <?php the_content(); ?>
+
+    <?php endwhile; endif; ?>
+
 <?php get_footer(); ?>
--- wp-content/themes/maxrenov/functions.php
+++ wp-content/themes/maxrenov/functions.php
@@ -6,6 +6,7 @@ add_theme_support('post-thumbnails');
 // Ajouter automatiquement le titre du site dans l'en-tête du site
 add_theme_support('title-tag');
 
+add_action('init', 'maxrenov_register_post_types');
 add_action('wp_enqueue_scripts', 'maxrenov_register_assets');
 
 function maxrenov_register_assets()
@@ -15,3 +16,26 @@ function maxrenov_register_assets()
     wp_enqueue_style('maxrenov-jumbotron', get_template_directory_uri() . '/style/jumbotron.css', [], $version);
 }
 
+function maxrenov_register_post_types()
+{
+    $labels = array(
+        'name' => 'Services',
+        'all_items' => 'Tous les services',
+        'singular_name' => 'Service',
+        'add_new_item' => 'Ajouter un service',
+        'edit_item' => 'Modifier le service',
+        'menu_name' => 'Services'
+    );
+
+    $args = array(
+        'labels' => $labels,
+        'public' => true,
+        'show_in_rest' => true,
+        'has_archive' => true,
+        'supports' => array('title', 'excerpt', 'thumbnail'),
+        'menu_position' => 20,
+        'menu_icon' => 'dashicons-hammer',
+    );
+
+    register_post_type('service', $args);
+}
