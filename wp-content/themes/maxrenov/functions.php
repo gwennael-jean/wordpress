9708ca93f9d01622d88cb95429d6d859fd7dbb30
Gwennael Jean <gwennael.jean@gmail.com>
2022-08-19 13:50:35 +0200

    feat(theme): création du jumbotron de la page d'accueil

--- wp-content/themes/maxrenov/functions.php
+++ wp-content/themes/maxrenov/functions.php
@@ -5,3 +5,13 @@ add_theme_support('post-thumbnails');
 
 // Ajouter automatiquement le titre du site dans l'en-tête du site
 add_theme_support('title-tag');
+
+add_action('wp_enqueue_scripts', 'maxrenov_register_assets');
+
+function maxrenov_register_assets()
+{
+    $version = '1.0';
+    wp_enqueue_style('maxrenov-style', get_stylesheet_uri(), [], $version);
+    wp_enqueue_style('maxrenov-jumbotron', get_template_directory_uri() . '/style/jumbotron.css', [], $version);
+}
+
--- wp-content/themes/maxrenov/header.php
+++ wp-content/themes/maxrenov/header.php
@@ -7,3 +7,13 @@
     </head>
     <body <?php body_class(); ?>>
         <?php wp_body_open(); ?>
+
+        <div class="jumbotron home-jumbotron">
+            <div class="jumbotron-shadow">
+                <div class="container">
+                    <h1 class="jumbotron-title">
+                        <img src="<?= get_template_directory_uri(); ?>/img/logo-white.png" alt="Logo">
+                    </h1>
+                </div>
+            </div>
+        </div>
